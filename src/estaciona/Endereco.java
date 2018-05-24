c8f5afdd80d6b18fd307cc7f95eb850867a614f9
Andreangelo <36939704+andreangelopp@users.noreply.github.com>
2018-05-24 09:52:07 -0300

    teste

---
+++ src/estaciona/Endereco.java
@@ -0,0 +1,89 @@
+
+package estaciona;
+
+
+public class Endereco {
+    private int id;
+    private Bairro bairro;
+    private String cep;
+    private String rua;
+    private String complemento;
+    public final int TAM_CEP = 9;
+    public final int TAM_COMPLEMENTO = 50;
+    public final int TAM_RUA = 100;
+    
+
+
+    @Override
+    public String toString(){
+        String texto;
+        texto = "rua: " + this.rua;
+        texto += "\nCEP: " + this.cep;
+        texto += "\nComplemento: " + this.complemento;
+        texto += "\nCidade: " + this.bairro.getCidade().getNome();
+        texto += "\nbEstado: " + this.bairro.getCidade().getEstado().getNome();
+        return texto;
+    }
+    
+    public Endereco(int id, Estado estado, Cidade cidade, Bairro bairro, String cep, String rua, String complemento) {
+        this.id = id;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.rua = rua;
+        this.complemento = complemento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId_endereco(int id) {
+        this.id = id;
+    }
+
+    public Estado getEstado() {
+        return bairro.getCidade().getEstado();
+    }
+
+    public Cidade getCidade() {
+        return bairro.getCidade();
+    }
+
+    public Bairro getBairro() {
+        return bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        if (cep.length() == TAM_CEP)
+            this.cep = cep;
+        else
+            throw new IllegalArgumentException("CEP inválido!");
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        if(rua.length() <= TAM_RUA && !rua.equals(""))
+            this.rua = rua;
+        else
+            throw new IllegalArgumentException("Rua inválida");
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        if(complemento.length() <= TAM_COMPLEMENTO)
+            this.complemento = complemento;
+        else
+            throw new IllegalArgumentException("Complemento inválido!");
+    }
+}
+
---
+++ src/estaciona/Vaga.java
@@ -0,0 +1,13 @@
+
+package estaciona;
+
+
+public class Vaga {
+    private int id;
+    private String status;
+    private int numero;
+    private int andar;
+    private String latitude;
+    private String longitude;
+    
+}
