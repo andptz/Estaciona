45e71204814efd27d49044a86a05b7228a96e1b9
Andreângelo <andreangelo.pp@gmail.com>
2018-06-07 10:30:17 -0300

    iniciando a construção da função recuperarTodos()

--- src/persistencia/locais/PersistenciaEstado.java
+++ src/persistencia/locais/PersistenciaEstado.java
@@ -3,6 +3,7 @@ package persistencia.locais;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -19,8 +20,8 @@ public class PersistenciaEstado {
         try {
             //verifica se as classe da biblioteca existem
          Class.forName("org.sqlite.JDBC");
-         //abre a conexao com o  banco de dados chamado lanchonete.
-         //esse banco de dado é em arquivo
+         //abre a conexao com o  banco de dados chamado BD_ESTACIONA.
+         //esse banco de dado é em arquivo???????????????
          connection  = DriverManager.getConnection("jdbc:sqlite:BD_ESTACIONA.db");
          System.out.println("Banco de dados aberto");
          stament = connection.createStatement();
@@ -40,7 +41,6 @@ public class PersistenciaEstado {
 
 
     public void salvarEstado(Estado estado) throws SQLException {
-        
         String sql = "INSERT INTO estado (nome) "+
                 "values ('"
                 +estado.getNome()+
@@ -50,11 +50,39 @@ public class PersistenciaEstado {
     }
 
 
-    public ArrayList recuperarTodos(){
+    public ArrayList recuperarTodos() throws ClassNotFoundException, SQLException{
         ArrayList<Estado> listaEstados = new ArrayList<Estado>();
         
-        String sql ="SELECT * FROM produto";
+        String sql ="SELECT * FROM estado";
         Connection connection = null;
         Statement stament = null;
+        
+        Class.forName("org.sqlite.JDBC");
+         //abre a conexao com o  banco de dados chamado BD_ESTACIONA.
+         //esse banco de dado é em arquivo ?????????????
+         connection = DriverManager.getConnection("jdbc:sqlite:BD_ESTACIONA.db");
+         System.out.println("Banco de dados aberto");
+         stament = connection.createStatement();
+         //exeucta a query no meu banco de dados
+         ResultSet rs = stament.executeQuery(sql);
+         while(rs.next()){
+             String nome = rs.getString("nome");
+             if(nome.equalsIgnoreCase(.HAMBURGUER)){
+                 Hamburguer hamburguer  = new Hamburguer();
+                 hamburguer.setDescricao(rs.getString("descricao"));
+                 hamburguer.setValor(rs.getDouble("valor"));
+                 listaEstados.add(estado);
+             }
+         }
+         stament.close();
+         //fecha a conexao com o banco de dados
+         connection.close();
+        
+        
+        
+        
+        return listaEstados;
     }
+    
+    
 }
