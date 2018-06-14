94b1221747e943c61e961bd70163679c9765464f
Andreangelo <andreangelo.pp@gmail.com>
2018-06-14 09:29:32 -0300

    atualização PersistenciaEndereco

--- src/persistencia/locais/PersistenciaEndereco.java
+++ src/persistencia/locais/PersistenciaEndereco.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package persistencia.locais;
 
-/**
- *
- * @author User
- */
+import java.sql.Connection;
+
+
 public class PersistenciaEndereco {
+    Connection conexao = null;
+    
+    public void criarEndereco(Cidade) throws SQLException{
+        String sql;
+        
+        conexao = ConexaoBD.conectar();
+        Statement statement = conexao.createStatement();
+        
+        //Se não existir uma cidade de mesmo nome da que se está tentando registrar, em um determinado estado, então o registro é permitido.
+        if (!statement.executeQuery(sql).first()) {
+            sql = String.format("INSERT INTO cidade (nome, FK_ESTADO_id) "
+                    + "VALUES (%s, %d)", cidade.getNome(), cidade.getFK_ESTADO_id());
+            
+            statement.executeUpdate(sql);
+            System.out.println("Nova cidade registrada com sucesso!");
+        }
+        else
+            System.out.println("Cidade já existente no banco de dados!");
+        
+        statement.close();
+        conexao.close();
+    }
     
 }
