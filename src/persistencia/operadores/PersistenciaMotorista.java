fbf6f1e2f02eaef8a54338550e55462261fddceb
Manel <Manel@172.17.104.65>
2018-06-15 11:30:03 -0300

    UP - Tela e Classes

--- src/persistencia/operadores/PersistenciaMotorista.java
+++ src/persistencia/operadores/PersistenciaMotorista.java
@@ -2,6 +2,8 @@
 package persistencia.operadores;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import modelo.operadores.Motorista;
@@ -16,16 +18,49 @@ public class PersistenciaMotorista {
     
     public void insertMotorista (Motorista motorista) throws SQLException{
  
+        int chaveEstrangeira = 0;
         
         conexao = ConexaoBD.conectar();
-        Statement statement = conexao.createStatement();
-        String sql = String.format("INSERT INTO pessoa (nome,telefone,email,senha) "
-        + "VALUES (%s, %s, %s, %s);", 
-                
-        motorista.getNomeCompleto(), motorista.getTelefone(), motorista.getEmail(),motorista.getSenha());
+        //========================================         
+        //Criar pessoa;
+        PreparedStatement ps = conexao.prepareStatement("Insert into pessoa(nome,telefone,email,senha)" + 
+                "values (?,?,?,?) returning id;");
         
-        statement.executeUpdate(sql);
-        statement.close();
+        ps.setString(1,motorista.getNomeCompleto());
+        ps.setString(2,motorista.getTelefone());
+        ps.setString(3,motorista.getEmail());
+        ps.setString(4,motorista.getSenha());
+        
+        //Executa SQL e guarda ID;
+        ResultSet rs = ps.executeQuery();
+        
+        //Verifica se possui algum retorno;
+        try{
+            if(rs.next()){
+                chaveEstrangeira = rs.getInt("id");
+                System.out.print("\nCHAVE: "+ chaveEstrangeira);
+            }
+        }catch(SecurityException e){
+            
+        }
+        
+        //=============================================
+        //Cria Pessoa Fisica;
+        ps = conexao.prepareStatement("Insert into pessoa_fisica(cpf,fk_pessoa_id)" + 
+                "values (?,?) returning id;");
+        
+        ps.setString(1,motorista.getCpf());
+        ps.setString(2,""+chaveEstrangeira);
+        
+        //Executa SQL e guarda ID;
+        rs = ps.executeQuery();
+        if(rs.next()){
+            chaveEstrangeira = rs.getInt("id");
+            System.out.print("\nCHAVE: "+ chaveEstrangeira);
+        }
+        
+        
+        ps.close();
         conexao.close();
     }
 
