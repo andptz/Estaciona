8c07e444897bcc62e0a313fb2806715445632ff6
Edmiltho <edmiltho@hotmail.com>
2018-06-18 14:26:09 -0300

    Add files via upload

---
+++ src/modelo/operacoes/Recarga.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.operacoes;
+
+import modelo.operadores.Motorista;
+
+public class Recarga {
+    private int valor;
+    private Forma_Pagamento formaPagamento;
+
+    public Recarga(Forma_Pagamento formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+    
+    
+
+
+    
+
+    
+
+    
+    
+    
+
+    
+    
+    
+}
