c470377068bfd6f2df3c0b5d220ce8c121b674c5
Edmiltho <edmiltho@hotmail.com>
2018-06-21 09:34:12 -0300

    Criada persistencia pagamento

---
+++ src/persistencia/operacoes/PersistenciaPagamento.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia.operacoes;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import persistencia.utilidade.ConexaoBD;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import modelo.operacoes.PagamentoEstacionamento;
+
+public class PersistenciaPagamento {
+    
+    Connection conexao = null;
+    
+    public void CriarPagamento(PagamentoEstacionamento pagamento) throws SQLException{
+        String sql;
+         
+        
+        conexao = ConexaoBD.conectar();
+        Statement statement = conexao.createStatement();
+        
+        sql = String.format("INSERT INTO pagamento (id_pagamento,valor,data,id_vaga,cpf)"
+        + "VALUES (%d, %f, %s, %d, %s)",pagamento.getId(),pagamento.getValor(),pagamento.getData(),pagamento.getVaga().getId(),pagamento.getMotorista().getCpf() );
+        statement.executeUpdate(sql);
+        System.out.println("Novo pagamento cadastrado!");
+        
+        statement.close();
+        conexao.close();
+         
+    }
+    
+}
