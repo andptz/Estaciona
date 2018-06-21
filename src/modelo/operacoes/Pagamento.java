c6732b9ece97c0f9aa51e96f56e30f945e3cc894
Edmiltho <edmiltho@hotmail.com>
2018-06-21 08:40:47 -0300

    Add files via upload

---
+++ src/modelo/operacoes/Pagamento.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.operacoes;
+
+
+public class Pagamento {
+    private int valor;
+
+    public Pagamento(int valor) {
+        this.valor = valor;
+    }
+    
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        if (valor!=0)
+            this.valor = valor;
+        else
+             throw new IllegalArgumentException("Valor vazio");
+    }
+    
+}
---
+++ src/modelo/operacoes/PagamentoReserva.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.operacoes;
+
+
+public class PagamentoReserva extends Pagamento{
+    public PagamentoReserva(int valor) {
+        super(valor);
+    }
+    
+}
