6c95aa11fd1b6fc150382cd826a7ba89a5d229be
Andreangelo <andreangelo.pp@gmail.com>
2018-06-27 10:29:49 -0300

    criada EnderecoTest

---
+++ test/modelo/locais/EnderecoTest.java
@@ -0,0 +1,194 @@
+
+package modelo.locais;
+
+import java.sql.SQLException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import persistencia.locais.PersistenciaCidade;
+import persistencia.locais.PersistenciaEndereco;
+import persistencia.locais.PersistenciaEstado;
+import persistencia.utilidade.ConexaoBD;
+
+
+public class EnderecoTest {
+    
+    public EnderecoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+
+//    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Endereco instance = new Endereco();
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
+//    @Test
+    public void testGetId() {
+        System.out.println("getId");
+        Endereco instance = new Endereco();
+        int expResult = 0;
+        int result = instance.getId();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
+//    @Test
+    public void testSetId() {
+        System.out.println("setId");
+        int id = 0;
+        Endereco instance = new Endereco();
+        instance.setId(id);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
+//    @Test
+    public void testGetLogradouro() {
+        System.out.println("getLogradouro");
+        Endereco instance = new Endereco();
+        String expResult = "";
+        String result = instance.getLogradouro();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
+//    @Test
+    public void testSetLogradouro() {
+        System.out.println("setLogradouro");
+        String logradouro = "";
+        Endereco instance = new Endereco();
+        instance.setLogradouro(logradouro);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
+//    @Test
+    public void testGetEstado() {
+        System.out.println("getEstado");
+        Endereco instance = new Endereco();
+        Estado expResult = null;
+        Estado result = instance.getEstado();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
+//    @Test
+    public void testGetCidade() {
+        System.out.println("getCidade");
+        Endereco instance = new Endereco();
+        Cidade expResult = null;
+        Cidade result = instance.getCidade();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
+//    @Test
+    public void testGetBairro() {
+        System.out.println("getBairro");
+        Endereco instance = new Endereco();
+        Bairro expResult = null;
+        Bairro result = instance.getBairro();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
+//    @Test
+    public void testGetCep() {
+        System.out.println("getCep");
+        Endereco instance = new Endereco();
+        String expResult = "";
+        String result = instance.getCep();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
+//    @Test
+    public void testSetCep() {
+        System.out.println("setCep");
+        String cep = "";
+        Endereco instance = new Endereco();
+        instance.setCep(cep);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
+//    @Test
+    public void testGetComplemento() {
+        System.out.println("getComplemento");
+        Endereco instance = new Endereco();
+        String expResult = "";
+        String result = instance.getComplemento();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
+//    @Test
+    public void testSetComplemento() {
+        System.out.println("setComplemento");
+        String complemento = "";
+        Endereco instance = new Endereco();
+        instance.setComplemento(complemento);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void insertEndereco(Endereco endereco) throws SQLException{
+        /*private int id;
+        private String logradouro;
+        private String complemento;
+        private String cep;
+        private Bairro bairro;
+        private Cidade cidade;
+        private Estado estado;*/
+        PersistenciaEndereco persistEndereco = new PersistenciaEndereco();
+        PersistenciaEstado persistEstado = new PersistenciaEstado();
+        PersistenciaCidade persistCidade = new ;
+        
+    }
+    
+}
+
